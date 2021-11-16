57dee5eeac224b087478aa8e4dafbd06d4ab6e6f
witoldmetel <witoldmetel@gmail.com>
2021-11-16 14:24:17 +0100

    mock search bar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import Fontisto from 'react-native-vector-icons/Fontisto';
 
 export function SearchBar() {
 	return (
-		<View>
-			<Text>SearchBar</Text>
+		<View style={styles.root}>
+			<Fontisto name="search" size={20} color="#858585" />
+			<Text style={styles.textIntput}>Search</Text>
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	root: {
+		backgroundColor: '#333',
+		flexDirection: 'row',
+		alignItems: 'center',
+		paddingHorizontal: 10,
+		height: 40,
+		borderRadius: 15,
+	},
+	textIntput: {
+		color: '#858585',
+		paddingLeft: 10,
+		fontSize: 20,
+	},
+});
