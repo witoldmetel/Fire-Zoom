324afe0aea01e2ce47b7e394a7a97ac8fc49cecf
witoldmetel <witoldmetel@gmail.com>
2021-11-16 12:16:20 +0100

    add header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+
+import Entypo from 'react-native-vector-icons/Entypo';
 
 export function Header() {
 	return (
-		<View>
-			<Text>Header</Text>
+		<View style={styles.root}>
+			<Entypo name="notification" size={30} color="#efe" />
+			<Text style={styles.heading}>Meet & Chat</Text>
+			<Entypo name="new-message" size={30} color="#efe" />
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	root: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		paddingVertical: 20,
+		paddingHorizontal: 10,
+	},
+	heading: {
+		color: '#efe',
+		fontSize: 20,
+		fontWeight: '700',
+	},
+});
