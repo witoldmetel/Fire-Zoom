63408c9c5bea7445763f5c9eefcb2be09b196ffd
witoldmetel <witoldmetel@gmail.com>
2022-11-15 19:25:25 +0100

    Run app

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
 
-import Navigation from './router/Navigation';
+import Navigation from './router/Navigation'
 
 export default function App() {
-	return <Navigation />
+  return <Navigation />
 }
+
